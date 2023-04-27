c399de67bfeb40894ad3f7abd5e89998b457a6c2
Crowdbotics <team@crowdbotics.com>
2023-04-27 18:58:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-disk-40261",
+  url: "https://wandering-disk-40261.botics.co",
+  api: "https://wandering-disk-40261.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
